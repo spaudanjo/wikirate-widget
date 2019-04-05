2d6c5df83188fbc441191cf7ae54d234d6515aa4
Daniel Spaude <daniel.spaude@zalando.de>
2019-04-05 13:05:17 +0200

    WIP

--- widget.js
+++ widget.js
@@ -1,65 +1,107 @@
-console.log('here we go')
+function renderPieChart(chartData) {
+    const myChart = echarts.init(document.getElementById('pie-chart'));
 
-var scope1DeferredData = fetch('https://wikirate.org/CDP+Scope_1_Emissions+Marks_and_Spencer_Group_plc+2019')
-	.then(data => {return data.json()})
+    option = {
+        backgroundColor: '#2c343c',
 
-var scope2DeferredData = fetch('https://wikirate.org/CDP+Scope_2_Emissions+Marks_and_Spencer_Group_plc+2019')
-	.then(data => {return data.json()})
-
-var scope3DeferredData = fetch('https://wikirate.org/CDP+Scope_3_Emissions+Marks_and_Spencer_Group_plc+2019')
-	.then(data => {return data.json()})
-
-var scopes
-Promise.all([scope1DeferredData, scope2DeferredData, scope3DeferredData])
-	.then(function(values) {console.log(values)})
-
-
-// extract companies
-
-
-
-// based on prepared DOM, initialize echarts instance
-//var myChart
-
-//var data
-
-//var oReq = new XMLHttpRequest();
-//oReq.addEventListener("load", reqListener);
-//oReq.open("GET", "https://wikirate.org/CDP+Scope_1_Emissions+Answer.json");
-//oReq.send();
-
-function reqListener () {
-	data = JSON.parse(this.responseText)
-}
-
-function chartPayscale() {
-	var labels = data.items.filter(item => item.company == document.getElementById('company').value).map(item => item.company)
-    var values = data.items.filter(item => item.company == document.getElementById('company').value).map(item => parseInt(item.value))
-
-    labels.push('London average payscale to worker')
-    values.push(200)
-
-	// specify chart configuration item and data
-    var option = {
-    	tooltip: {
-	        trigger: 'axis'
-	    },
-        xAxis: {
-            data: labels
+        title: {
+            text: `Emissions of '${chartData.companyName}'`,
+            left: 'center',
+            top: 20,
+            textStyle: {
+                color: '#ccc'
+            }
         },
-        yAxis: {},
-        series: [{
-            type: 'bar',
-            data: values
-        }]
+
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+
+        series: [
+            {
+                name: 'Scope 1 and 2 Emissions',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '50%'],
+                data: chartData.chartSeriesData,
+                roseType: 'radius',
+                label: {
+                    normal: {
+                        textStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        }
+                    }
+                },
+                labelLine: {
+                    normal: {
+                        lineStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        },
+                        smooth: 0.2,
+                        length: 10,
+                        length2: 20
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        color: '#c23531',
+                        shadowBlur: 200,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                },
+
+                animationType: 'scale',
+                animationEasing: 'elasticOut',
+                animationDelay: function (idx) {
+                    return Math.random() * 200;
+                }
+            }
+        ]
     };
 
     // use configuration item and data specified to show chart
     myChart.setOption(option);
 }
 
-// function renderOn(elementId) {
-	// pieChart = echarts.init(document.getElementById(elementId));
+function rebuildChartForCompany(normalizedCompanyName) {
+    const scope1DeferredData = fetch(`https://wikirate.org/CDP+Scope_1_Emissions+${normalizedCompanyName}+2019.json`)
+        .then(data => { return data.json() });
 
-	
-// }
+    const scope2DeferredData = fetch(`https://wikirate.org/CDP+Scope_2_Emissions+${normalizedCompanyName}+2019.json`)
+        .then(data => { return data.json() });
+
+    Promise
+        .all([scope1DeferredData, scope2DeferredData])
+        .then(emissionData => {
+
+            console.log(emissionData); 
+            const humanizedCompanyName = emissionData[0].company;
+
+            const emissionValuesPerScope = emissionData.map(data => ({
+                value: data.value,
+                name: data.metric
+            }));
+
+            const chartData = {
+                companyName: humanizedCompanyName,
+                chartSeriesData: emissionValuesPerScope
+            };
+
+            renderPieChart(chartData);
+        });
+}
+
+function processForm(e) {
+    if (e.preventDefault) e.preventDefault();
+    const companyName = document.getElementById("company-name").value;
+    rebuildChartForCompany(companyName);
+};
+
+const companySearchForm = document.getElementById('company-search');
+
+if (companySearchForm.attachEvent) {
+    companySearchForm.attachEvent("submit", processForm);
+} else {
+    companySearchForm.addEventListener("submit", processForm);
+} 
