3a0c2822e9455c1f4ee9158f1d563081fe7309b3
Daniel Spaude <daniel.spaude@zalando.de>
2020-04-15 22:44:13 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ const WikirateWidget = {
         Vue.config.productionTip = false
         return new Vue({
             el: element,
-            props: ['numberOfTopAnswersToShow', 'years'],
             render: h => h(App, { props: { numberOfTopAnswersToShow, answerEndpoints } })
         });
     }
@@ -29,12 +28,5 @@ declare global {
         window.WikirateWidget = WikirateWidget;
     }
 })(window);
-
-
-WikirateWidget.renderWidget({
-    element: "#app", 
-    numberOfTopAnswersToShow: 10, 
-    answerEndpoints: [2018, 2017].map(year => `https://wikirate.org/Commons+Greenhouse_Gas_Emissions_Scope_1_and_2_combined+Answer.json?filter%5Bproject%5D=Question+Widget%3A+GHG+emissions&view=compact&filter[year]=${year}`)
-});
 
 export default WikirateWidget; 
